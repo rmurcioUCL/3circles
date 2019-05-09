48b52f81d49f6c798eb1b0ff859e0b9a553e4866
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-05-09 18:03:31 +0200

    add_category_tree_file

--- juste/msoa_flows.R
+++ juste/msoa_flows.R
@@ -7,12 +7,18 @@ library(sf)
 library(cartography)
 library(dplyr)
 library(ggplot2)
+library(igraph)
 
 
 msoa = st_read(dsn = '../London_Data/ESRI',layer='MSOA_2011_London_gen_MHW')
 
 flows = as.tbl(read.csv('../../../Data/Foursquare/movements_v2/London_movements_v2.csv',header=F))
 names(flows)<-c('from','to','month','window','count')
+
+flows$id = paste0(flows$from,flows$to)
+
+counts = flows %>% group_by(id,month) %>% summarize(count=n())
+
 venues = as.tbl(read.csv('../../../Data/Foursquare/venues_v2/London_venue_info_v2.csv'))
 venuepoints=list();for(i in 1:nrow(venues)){venuepoints[[i]]=st_point(as.matrix(venues[i,c("lng","lat")]))}
 venuesf = st_sf(venues,geometry=venuepoints)
@@ -104,6 +110,49 @@ layoutLayer(title = "Aggregated flows between MSOA",
             frame = FALSE, col = "grey25", coltitle = "white",
             tabtitle = TRUE)
 dev.off()
+
+
+
+######
+# network topology in time
+
+# aggreg to graph
+sflows <- over_flows %>% group_by(from_msoa,to_msoa,window,month) %>% # filter(!is.na(from_msoa)&!is.na(to_msoa)) %>%
+  summarize(count=sum(count))
+sflows$from_msoa = as.character(sflows$from_msoa);sflows$to_msoa = as.character(sflows$to_msoa)
+sflows = sflows %>% filter(!is.na(from_msoa)&!is.na(to_msoa))
+sflows$weight = sflows$count
+sflows$count = NULL
+
+graph <- graph_from_data_frame(as.data.frame(sflows))
+
+computeGraphMeasures <- function(g){
+  degs = degree(g ,normalized = T)
+  avgDeg = mean(degs)
+  degs = degs[degs>median(degs)]
+  alphaDeg = summary(lm(data=data.frame(rank=log(1:length(degs)),deg=log(sort(degs,decreasing = T))),deg~rank))$adj.r.squared
+  return(c(avgDeg=avgDeg,alphaDeg=alphaDeg))
+}
+
+measures=c()
+for(month in unique(E(graph)$month)){
+  show(month)
+  currentgraph = subgraph.edges(graph,which(E(graph)$month==month))
+  mes = computeGraphMeasures(currentgraph)
+  measures=append(measures,c(value=mes['avgDeg'],var='avgDeg',month=month))
+  measures=append(measures,c(value=mes['alphaDeg'],var='alphaDeg',month=month))
+}
+
+measuresdf=data.frame()
+names(measures)=c('value','var','month')
+measures$value<- as.numeric(measures$value)
+
+g=ggplot(measures,aes(x=month,y=value,color=var,group=var))
+g+geom_point()+geom_line()
+
+
+
+
 
 
 
---
+++ metadata/readme.md
@@ -0,0 +1,8 @@
+
+====
+`categories.json` : venues category tree (version 2019/05/09) retrieved through the userless api at https://api.foursquare.com/v2/venues/categories?client_id=ID&client_secret=SECRET&v=20190509
+
+changelog of categories : https://developer.foursquare.com/docs/resources/categories-changelog
+
+
+
